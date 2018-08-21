04ed703b81fc321d1de164593eaee47c31f10c5f
micuat <micuat@gmail.com>
2018-08-21 19:26:08 +0900

    temple

---
+++ templeTex/js/frag.glsl
@@ -0,0 +1,57 @@
+// https://www.shadertoy.com/view/4llGWM
+
+#ifdef GL_ES
+precision mediump float;
+precision mediump int;
+#endif
+
+varying vec3 vertNormal;
+varying vec3 vertLightDir;
+varying vec4 vertColor;
+varying vec4 vertTexCoord;
+
+uniform float iTime, lfo0, lfo1, lfo2, lfo3;
+uniform float vMirror;
+uniform float centerDirection;
+uniform vec3 bgColor0, bgColor1;
+
+uniform sampler2D texture;
+uniform sampler2D pgTex;
+uniform sampler2D waveTex;
+uniform sampler2D backTex;
+
+vec2 iRes = vec2(1280, 560);
+
+float PI = 3.14159265359;
+
+void main() {
+  vec2 fragCoord = vertTexCoord.st;
+	vec2 nFragCoord = fragCoord.st - vec2(0.5);
+
+	float lfc = length(nFragCoord);
+	float afc = atan(nFragCoord.t, nFragCoord.s);
+
+	vec4 pgCol = texture(pgTex, vertTexCoord.st);
+	vec4 backCol;
+  float dth = 0.02;// * lfo1;// * (2.0 + lfc);// * (lfo0 * 2.0 - 1.0);
+	backCol = texture(backTex, vec2(0.5) + vec2(lfc*cos(afc + dth), lfc*sin(afc + dth)));
+
+  float x = fragCoord.x - 0.5;
+  float y = 1.0 - fragCoord.y;
+	vec4 fragCol0 = vec4(vec3(pow(sin(y * (lfo0 * 10.0 + 50.0) - iTime - x * 10.0) * cos(x * (lfo0 * 5.0 + 10.0)), 4.0)), 1.0);//texture(waveTex, vec2(y, 0.5));
+	fragCol0.rgb *= mix(bgColor0, vec3(1.0), 1.0 - lfo2);
+
+  y = y * 2.0 - floor(y * 2.0);
+	vec4 fragCol1 = texture(waveTex, vec2(y, 0.5));
+	fragCol1.rgb *= bgColor1 * 0.5;
+
+	float alpha = pgCol.r;
+
+	vec4 finalColor = pgCol*1.0;// + fragCol0 * 1.0 + fragCol1 * 0.0;
+  if(lfo0 > 0.75)
+  	finalColor.rgb = mix(finalColor.rgb, backCol.rgb, (lfo0 - 0.5) * 2.0);
+  else
+  	finalColor.rgb = mix(finalColor.rgb, backCol.rgb, 0.5);
+	gl_FragColor = finalColor;
+	// gl_FragColor = pgCol;
+}
---
+++ templeTex/js/sketch.js
@@ -0,0 +1,205 @@
+// https://stackoverflow.com/questions/17242144/javascript-convert-hsb-hsv-color-to-rgb-accurately
+function HSVtoRGB(h, s, v) {
+  var r, g, b, i, f, p, q, t;
+  if (arguments.length === 1) {
+      s = h.s, v = h.v, h = h.h;
+  }
+  i = Math.floor(h * 6);
+  f = h * 6 - i;
+  p = v * (1 - s);
+  q = v * (1 - f * s);
+  t = v * (1 - (1 - f) * s);
+  switch (i % 6) {
+      case 0: r = v, g = t, b = p; break;
+      case 1: r = q, g = v, b = p; break;
+      case 2: r = p, g = v, b = t; break;
+      case 3: r = p, g = q, b = v; break;
+      case 4: r = t, g = p, b = v; break;
+      case 5: r = v, g = p, b = q; break;
+  }
+  return {
+      r: r,
+      g: g,
+      b: b
+  };
+}
+
+var frontPg;
+var backPg;
+var wavePg;
+
+var s = function (p) {
+  let name;
+  let transformFunc;
+  let startFrame;
+  let shader, feedbackShader;
+  let pg;
+  let targetI;
+
+  p.setup = function () {
+    name = p.folderName;
+    p.createCanvas(1280/2, 560/2);
+    p.frameRate(60);
+    startFrame = p.frameCount;
+
+    pg = p.createGraphics(p.width, p.height, p.P3D);
+    if(frontPg == undefined)
+      frontPg = p.createGraphics(p.width, p.height, p.P3D);
+    if(backPg == undefined)
+      backPg = p.createGraphics(p.width, p.height, p.P3D);
+    if(wavePg == undefined)
+      wavePg = p.createGraphics(100, 100);
+    shader = p.loadShader(p.sketchPath(name + "/frag.glsl"));
+  }
+
+  function getCount() { return p.frameCount - startFrame };
+
+  p.draw = function () {
+    p.background(0);
+    p.stroke(255);
+
+    if(getCount() % 60 == 0) {
+      shader = p.loadShader(p.sketchPath(name + "/frag.glsl"));
+    }
+    if(getCount() % 240 == 0) {
+      targetI = Math.floor(p.random(0, 6));
+    }
+
+    let t = getCount() / 60.0;
+
+    let colorSc = [
+      [0, 255, 150],
+      [0, 155, 50],
+      [50, 100, 255],
+      [0, 70, 155],
+      [230, 230, 100],
+      [205, 70, 0],
+      [20, 205, 200],
+      [135, 30, 0]
+    ]
+    function fillAt(i) {
+      pg.fill(colorSc[i][0], colorSc[i][1], colorSc[i][2]);
+    }
+    function strokeAt(i) {
+      pg.stroke(colorSc[i][0], colorSc[i][1], colorSc[i][2]);
+    }
+
+    let lfo0 = Math.cos(t * Math.PI * 0.25) * 0.5 + 0.5;
+    let tween = (t * 0.25 % 1.0) * 2.0 - 1.0;
+    let tweenp = 2.0;
+    if (tween < 0) {
+      tween = Math.pow(p.map(tween, -1, 0, 0, 1), tweenp) * 0.5;
+    }
+    else {
+      tween = 1.0 - Math.pow(p.map(tween, 0, 1, 1, 0), tweenp) * 0.5;
+    }
+    function tweenPowReturn () {
+      if (tween < 0.5) {
+        return p.map(tween, 0, 0.5, 0.0, 1.0);
+      }
+      else {
+        return p.map(tween, 0.5, 1.0, 1.0, 0.0);
+      }
+    }
+
+    pg.beginDraw();
+    pg.noStroke();
+
+    fillAt(7);
+    pg.rect(0, 0, p.width, p.height);
+    pg.translate(p.width / 2.0, p.height / 2.0);
+
+    // let shape = p.createShape();
+    for(let j = -1; j <= 1; j++) {
+      pg.pushMatrix();
+      pg.translate(j * 250, 0);
+      if(j == 0)
+        pg.rotate(tween * Math.PI);
+      else
+        pg.rotate(-tween * Math.PI);
+      for(let icolor = 0; icolor < 2; icolor++) {
+        if(icolor == 0)
+          fillAt((j + 1) % 2 * 2 + 1);
+        else {
+          fillAt((j + 1) % 2 * 2);
+        }
+        for(let i = 0; i < 6; i++) {
+          let tw = 0;
+          if(i == targetI) tw = tweenPowReturn();
+
+          pg.pushMatrix();
+          pg.rotate(i / 6.0 * p.TWO_PI);
+
+          if(icolor == 1)
+          pg.scale(0.6, 0.95);
+
+          pg.pushMatrix();
+          pg.rotate(1.5 / 6.0 * p.TWO_PI);
+          pg.translate(p.map(tw, 0.0, 1.0, -0.0, -80.0), 0);
+          pg.beginShape();
+          let da = 0.15;
+          let r = 100;
+          let angle = 0.0;
+          r = 100;
+          pg.vertex(r * Math.cos(angle - da), r * Math.sin(angle - da));
+          r = 108;
+          if(icolor == 1) r = 104;
+          pg.vertex(r * Math.cos(angle), r * Math.sin(angle));
+          r = 100;
+          pg.vertex(r * Math.cos(angle + da), r * Math.sin(angle + da));
+          r = 30;
+          angle = (i + 0.5) / 6.0 * p.TWO_PI;
+
+          pg.endShape();
+          pg.popMatrix();
+          // pg.rotate(-1.5 / 6.0 * p.TWO_PI);
+          let w = p.map(tw, 0.0, 1.0, 79.0, -0.0);
+          pg.rect(-15, 20, 30, w);
+          pg.rect(-15, 20, 30, -20);
+          pg.popMatrix();
+        }
+      }
+      pg.popMatrix();
+    }
+    // pg.shape(shape, 0, 0);
+    // p.image(pg, 0, 0);
+    pg.endDraw();
+
+    wavePg.beginDraw();
+    wavePg.strokeWeight(2);
+    for(let i = 0; i < 100; i++) {
+      let y = Math.pow((p.noise(((i*0.1 - t * (2.0))), t * -0.0)), 4.0) * 250;
+      wavePg.stroke(y);
+      wavePg.line(i, 0, i, 100);
+    }
+    wavePg.endDraw();
+
+    shader.set("iTime", t);
+    shader.set("lfo0", lfo0);
+    shader.set("lfo1", Math.cos(t * 1.0) * 0.5 + 0.5);
+    shader.set("lfo2", Math.pow(Math.sin(t) * 0.5 + 0.5, 4.0));
+    shader.set("vMirror", p.mouseX/800.0);
+    let centerDirection = 0.99;//p.map(Math.sin(t * 0.1), -1, 1, 0.99, 1.0);
+    shader.set("centerDirection", centerDirection);
+    let rgb = HSVtoRGB((t * 0.1) % 1.0, 1.0, 1.0);
+    shader.set("bgColor0", rgb.r, rgb.g, rgb.b);
+    rgb = HSVtoRGB((t + 0.5) % 1.0, 1.0, 1.0);
+    shader.set("bgColor1", rgb.r, rgb.g, rgb.b);
+    shader.set("pgTex", pg);
+    shader.set("waveTex", wavePg);
+    shader.set("backTex", backPg);
+    frontPg.beginDraw();
+    frontPg.filter(shader);
+    frontPg.endDraw();
+
+    p.resetShader();
+    p.image(frontPg, 0, 0);
+    p.syphonServer.sendImage(frontPg);
+
+    let intermediatePg = frontPg;
+    frontPg = backPg;
+    backPg = intermediatePg;
+  }
+};
+
+var p080 = new p5(s);
