510946e79b55ac7378b36bcf06aa3b5bffc312ba
micuat <micuat@gmail.com>
2018-08-29 21:49:32 +0900

    window size

--- eyes/src/main.cpp
+++ eyes/src/main.cpp
@@ -5,7 +5,7 @@
 int main( ){
     ofGLWindowSettings settings;
     settings.setGLVersion(3, 2);    // <--- ofxShadertoy NEEDS the GL Programmable Renderer
-    settings.setSize(1920, 768);
+    settings.setSize(1280, 720);
     ofCreateWindow(settings);       // <-------- setup the GL context
     
     // this kicks off the running of my app
