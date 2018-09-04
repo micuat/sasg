aba9382daa0b7f1eee0764e8ca4a4cd9c2fbca5c
micuat <micuat@gmail.com>
2018-09-04 11:12:51 +0900

    lfo!!!

---
+++ shadowUnity/Assets/Scripts/LFOPosition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LFOPosition : BaseLFO
+{
+    Transform tr;
+    Vector3 orgPosition = new Vector3();
+
+    void Start()
+    {
+        tr = gameObject.transform;
+        orgPosition.x = tr.position.x;
+        orgPosition.y = tr.position.y;
+        orgPosition.z = tr.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tr.position = new Vector3(orgPosition.x + lfo0,
+                                  orgPosition.y + lfo1,
+                                  orgPosition.z + lfo2);
+    }
+}
---
+++ shadowUnity/Assets/Scripts/LFORotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LFORotation : BaseLFO {
+    Transform tr;
+    Vector3 orgRotation = new Vector3();
+
+    void Start()
+    {
+        tr = gameObject.transform;
+        orgRotation.x = tr.eulerAngles.x;
+        orgRotation.y = tr.eulerAngles.y;
+        orgRotation.z = tr.eulerAngles.z;
+    }
+
+    // Update is called once per frame
+	void Update () {
+        base.UpdateTime();
+        tr.eulerAngles = new Vector3(orgRotation.x + 30 * Easing.Bounce.InOut(t0),
+                                     orgRotation.y + 30 * Easing.Bounce.InOut(t1),
+                                     orgRotation.z + 30 * Easing.Bounce.InOut(t2));
+	}
+}
